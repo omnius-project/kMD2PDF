1b78a0baeba7cda73c901ed7b3bf37ab4079b01f
woojiahao <woojiahao1234@gmail.com>
2019-02-16 07:58:30 +0800

    Added documentations for FontFamily

--- src/main/kotlin/me/chill/style/FontFamily.kt
+++ src/main/kotlin/me/chill/style/FontFamily.kt
@@ -1,15 +1,29 @@
 package me.chill.style
 
-class FontFamily(vararg fonts: String) {
+/**
+ * Handles the font families to be rendered. A set of [fonts] can be given during object
+ * creation to load into the [FontFamily] before hand.
+ */
+class FontFamily(private vararg val fonts: String) {
+
   private val fontFamily = mutableListOf<String>()
 
   init {
     fontFamily.addAll(fonts)
   }
 
+  /**
+   * Unary + used in DSL to add a new font to the [FontFamily].
+   */
   operator fun String.unaryPlus() = fontFamily.add(this)
 
+  /**
+   * Clears fonts stored in [FontFamily].
+   */
   fun emptyFontFamily() = fontFamily.clear()
 
+  /**
+   * Converts the [FontFamily] to the string format appropriate for CSS styles to use.
+   */
   fun getFontFamilyString() = fontFamily.joinToString(", ") { "\"$it\"" }
 }
