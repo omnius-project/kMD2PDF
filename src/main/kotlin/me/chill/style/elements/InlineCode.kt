e5d7c8c5b95ee3f45a1358a02245cfbc0348a2ba
woojiahao <woojiahao1234@gmail.com>
2019-02-16 16:55:11 +0800

    Added unit testing for the StyleUtility functions

--- src/main/kotlin/me/chill/style/elements/InlineCode.kt
+++ src/main/kotlin/me/chill/style/elements/InlineCode.kt
@@ -5,14 +5,14 @@ import me.chill.utility.px
 import java.awt.Color
 
 /**
- * <code> element styles.
+ * Inline <code> element styles.
  */
 class InlineCode(
   fontSize: Double = 16.0,
   fontFamily: FontFamily = FontFamily("monospace")
 ) : Element(fontSize, fontFamily) {
   override var fontColor: Color = Color.decode("#FF3D00")
-  override var backgroundColor: Color? = Color.decode("#FAFAFA")
+  override var backgroundColor: Color? = Color.decode("#F5F5F5")
   var borderRadius = 5.0
   var padding = 3.0
 }
