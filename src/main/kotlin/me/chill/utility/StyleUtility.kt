5e581f254d1f02ef9f07eee38226f1e68567afb1
woojiahao <woojiahao1234@gmail.com>
2019-02-16 16:22:22 +0800

    Created helper function to generate Color objects from hexcodes

--- src/main/kotlin/me/chill/utility/StyleUtility.kt
+++ src/main/kotlin/me/chill/utility/StyleUtility.kt
@@ -1,4 +1,22 @@
 package me.chill.utility
 
+import java.awt.Color
+
+private val hexColorRegex = Regex("#|([0-9A-Fa-f].)")
+
+/**
+ * Returns a number in pixel format.
+ */
 val <T : Number> T.px
   get() = "${this}px"
+
+/**
+ * Returns a matching [Color] based on the given [hexCode].
+ */
+fun c(hexCode: String) =
+  with(hexColorRegex.find(hexCode)) {
+    require(this != null) { "Hex code not found" }
+    require(value.length == 8) { "Hex code ($value) cannot have more than 8 characters" }
+
+    Color.decode("#$value")
+  }
