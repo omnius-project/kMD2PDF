b0f2522ba2a7bd54bcd13594986563a4ef90cd06
woojiahao <woojiahao1234@gmail.com>
2019-02-16 14:11:34 +0800

    Added documentation for MarkdownRenderer

--- src/main/kotlin/me/chill/rendering/MarkdownRenderer.kt
+++ src/main/kotlin/me/chill/rendering/MarkdownRenderer.kt
@@ -1,5 +1,9 @@
 package me.chill.rendering
 
+import kotlinx.html.body
+import kotlinx.html.html
+import kotlinx.html.stream.appendHTML
+import kotlinx.html.unsafe
 import me.chill.MarkdownDocument
 import me.chill.style.GenericPDFStyle
 import me.chill.utility.getFontDirectories
@@ -7,12 +11,9 @@ import org.commonmark.renderer.html.HtmlRenderer
 import org.xhtmlrenderer.pdf.ITextRenderer
 import java.io.File
 import java.io.FileOutputStream
-import kotlinx.html.*
-import kotlinx.html.dom.document
-import kotlinx.html.stream.appendHTML
 
 /**
- * Renders a markdown file to a PDF.
+ * Converts a [markdownDocument] to a PDF, applying the supplied [style] to the PDF.
  */
 class MarkdownRenderer(
   private val markdownDocument: MarkdownDocument,
@@ -24,6 +25,10 @@ class MarkdownRenderer(
     .attributeProviderFactory { HTMLStyleForPDFProvider(style) }
     .build()
 
+  /**
+   * Converts the [markdownDocument] into HTML for the PDF to render.
+   * [style] is rendered along with the HTML as inline styles.
+   */
   fun toHTML() = StringBuilder().appendHTML().html {
     body {
       unsafe {
@@ -32,9 +37,12 @@ class MarkdownRenderer(
     }
   }.toString()
 
+  /**
+   * Converts the given [markdownDocument] to a PDF, saving it at the location
+   * specified by [targetFile].
+   */
   fun constructPDF(targetFile: File) {
     with(ITextRenderer()) {
-      println(toHTML())
       setDocumentFromString(toHTML())
       loadFontDirectories()
       layout()
@@ -42,6 +50,13 @@ class MarkdownRenderer(
     }
   }
 
+  /**
+   * Loads all available font directories into an [ITextRenderer] to be used with
+   * the styles.
+   *
+   * If no font directories are located, a warning is made to the user, selected
+   * fonts will not render.
+   */
   private fun ITextRenderer.loadFontDirectories() {
     val fontDirectories = getFontDirectories()
     val hasNoFontDirectory = fontDirectories.none { File(it).exists() }
