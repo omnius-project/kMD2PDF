b550256f485f985e8ec71f425f274a97b68ca8ec
woojiahao <woojiahao1234@gmail.com>
2019-02-16 08:39:44 +0800

    Added unit tests for FontFamily

---
+++ src/test/kotlin/me/chill/style/FontFamilyTest.kt
@@ -0,0 +1,55 @@
+package me.chill.style
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class FontFamilyTest {
+  @Test
+  fun `Creating FontFamily with empty fonts parameter leaves fonts empty`() {
+    val fontFamily = FontFamily()
+    fontFamily.checkFontFamilySize(0)
+  }
+
+  @Test
+  fun `Creating FontFamily with some fonts will load those fonts into the FontFamily`() {
+    val fontFamily = FontFamily("Roboto", "Arial", "Consolas")
+    fontFamily.checkFontList("Roboto", "Arial", "Consolas")
+  }
+
+  @Test
+  fun `Emptying FontFamily will clear any fonts loaded`() {
+    val fontFamily = FontFamily("Fira Code", "Monaco", "Droid Sans Mono", "DejaVu Sans Mono")
+    fontFamily.checkFontFamilySize(4)
+    fontFamily.emptyFontFamily()
+    fontFamily.checkFontFamilySize(0)
+  }
+
+  @Test
+  fun `DSL FontFamily can add new font using unary plus operator`() {
+    val fontFamily = FontFamily("Lato")
+    fontFamily.checkFontList("Lato")
+    fontFamily.addFont {
+      +"Roboto"
+    }
+    fontFamily.checkFontList("Lato", "Roboto")
+  }
+
+  @Test
+  fun `FontFamily toString will return the font family in CSS font-family format`() {
+    val fontFamily = FontFamily("Fira Code", "Roboto", "Raleway")
+    val expectedString = "\"Fira Code\", Roboto, Raleway"
+    assertEquals(expectedString, fontFamily.toString())
+  }
+
+  private fun FontFamily.checkFontFamilySize(expectedSize: Int) {
+    assertEquals(expectedSize, getFonts().size)
+  }
+
+  private fun FontFamily.checkFontList(vararg fonts: String) {
+    assertEquals(fonts.toList(), getFonts())
+  }
+
+  private fun FontFamily.addFont(func: FontFamily.() -> Unit) {
+    func()
+  }
+}
