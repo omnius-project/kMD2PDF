47014d81e4b1770ec7b8b7f981d20d17cd5a660b
woojiahao <woojiahao1234@gmail.com>
2019-02-17 09:03:41 +0800

    Refactored PDFStyleProvider to have PDFStyleProvider#setStyle setup the base Element styles for each node

--- src/main/kotlin/me/chill/rendering/PDFStyleProvider.kt
+++ src/main/kotlin/me/chill/rendering/PDFStyleProvider.kt
@@ -2,10 +2,12 @@ package me.chill.rendering
 
 import me.chill.style.InlineStyleRenderer
 import me.chill.style.PDFStyle
+import me.chill.style.elements.Element
+import me.chill.utility.cssColor
 import me.chill.utility.px
+import org.bouncycastle.crypto.tls.ExtensionType
 import org.commonmark.node.*
 import org.commonmark.renderer.html.AttributeProvider
-import java.awt.Color
 import org.commonmark.renderer.html.HtmlRenderer
 
 /**
@@ -16,64 +18,37 @@ class PDFStyleProvider(private val style: PDFStyle) : AttributeProvider {
 
   override fun setAttributes(node: Node, tagName: String, attributes: MutableMap<String, String>) {
     val inlineStyleRenderer = InlineStyleRenderer()
-    with(node) {
-      when (this) {
-        // TODO: Shift into separate methods
-        is Heading -> {
+
+    when (node) {
+      is Heading -> inlineStyleRenderer.setStyle(style.matchHeaderLevel(node.level))
+
+      is Code -> {
+        with(style.inlineCode) {
           inlineStyleRenderer
-            .attribute("font-family", style.matchHeaderLevel(level) { it.fontFamily.toString() })
-            .attribute("color", style.matchHeaderLevel(level) { it.fontColor?.cssColor() })
-            .attribute("font-size", style.matchHeaderLevel(level) { it.fontSize.px })
-        }
-
-        is Code -> {
-          with(style.inlineCode) {
-            inlineStyleRenderer
-              .attribute("font-family", fontFamily.toString())
-              .attribute("color", fontColor?.cssColor())
-              .attribute("background-color", backgroundColor?.cssColor())
-              .attribute("font-size", fontSize.px)
-              .attribute("border-radius", borderRadius.px)
-              .attribute("padding", padding.px)
-          }
-        }
-
-        is StrongEmphasis -> {
-          with(style.bold) {
-            inlineStyleRenderer
-              .attribute("font-family", fontFamily.toString())
-              .attribute("font-weight", fontWeight.name.toLowerCase())
-              .attribute("color", fontColor?.cssColor())
-              .attribute("background-color", backgroundColor?.cssColor())
-              .attribute("font-size", fontSize.px)
-          }
-        }
-
-        is Paragraph -> {
-          with(style.paragraph) {
-            inlineStyleRenderer
-              .attribute("font-family", fontFamily.toString())
-              .attribute("color", fontColor?.cssColor())
-              .attribute("background-color", backgroundColor?.cssColor())
-              .attribute("font-size", fontSize.px)
-          }
-        }
-
-        is Link -> {
-          with(style.link) {
-            inlineStyleRenderer
-              .attribute("font-family", fontFamily.toString())
-              .attribute("color", fontColor?.cssColor())
-              .attribute("background-color", backgroundColor?.cssColor())
-              .attribute("font-size", fontSize.px)
-              .attribute("text-decoration", textDecoration.name.toLowerCase())
-          }
+            .setStyle(this)
+            .attribute("border-radius", borderRadius.px)
+            .attribute("padding", padding.px)
         }
       }
 
-      attributes["style"] = inlineStyleRenderer.renderStyle()
+      is StrongEmphasis -> inlineStyleRenderer.setStyle(style.bold)
+
+      is Paragraph -> inlineStyleRenderer.setStyle(style.paragraph)
+
+      is Link -> inlineStyleRenderer.setStyle(style.link)
     }
+
+    attributes["style"] = inlineStyleRenderer.renderStyle()
   }
 
-  private fun Color.cssColor() = "rgb($red, $green, $blue)"
+  private fun InlineStyleRenderer.setStyle(element: Element) =
+    with(element) {
+      attribute("font-size", fontSize.px)
+      attribute("font-family", fontFamily)
+      attribute("color", fontColor?.cssColor())
+      attribute("background-color", backgroundColor?.cssColor())
+      attribute("font-weight", fontWeight.name.toLowerCase())
+      attribute("text-decoration", textDecoration.toCss())
+      attribute("border", border)
+    }
 }
