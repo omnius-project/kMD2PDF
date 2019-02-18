28462f88368877e17a0a5c9ae411330fe935dc94
woojiahao <woojiahao1234@gmail.com>
2019-02-18 12:02:11 +0800

    BlockQuote now has some default styling using the new BorderBox

--- src/main/kotlin/me/chill/style/elements/BlockQuote.kt
+++ src/main/kotlin/me/chill/style/elements/BlockQuote.kt
@@ -1,5 +1,8 @@
 package me.chill.style.elements
 
+import me.chill.style.Border
+import me.chill.style.BorderBox
+import me.chill.style.Box
 import me.chill.style.FontFamily
 import me.chill.style.FontFamily.BaseFontFamily.SANS_SERIF
 import me.chill.utility.c
@@ -8,5 +11,13 @@ class BlockQuote(
   fontSize: Double = 16.0,
   fontFamily: FontFamily = FontFamily(SANS_SERIF)
 ) : Element(fontSize, fontFamily) {
-  override var backgroundColor = c("ECEFF1")
+  override var backgroundColor = c("BBDEFB")
+  override var padding: Box<Double>? = Box(10.0, 20.0)
+  override var margin: Box<Double>? = Box(0.0)
+  override var border = BorderBox(
+    Border(),
+    Border(),
+    Border(),
+    Border(5.0, Border.BorderStyle.SOLID, c("1565C0"))
+  )
 }
