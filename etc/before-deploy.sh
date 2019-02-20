4136b19680fbcc55fc827794e5cc5ba14e5d2e39
woojiahao <woojiahao1234@gmail.com>
2019-02-20 16:51:56 +0800

    Configured Travis to auto push to Maven central

---
+++ etc/before-deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+	openssl aes-256-cbc -K $encrypted_e1d4b2f3c747_key -iv $encrypted_e1d4b2f3c747_iv -in codesigning.asc.enc -out etc/codesigning.asc -d
+	gpg --fast-import etc/codesigning.asc
+fi
