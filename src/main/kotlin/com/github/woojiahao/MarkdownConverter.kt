2e079c99a7db7aaf2ea2a7dc2457096187948714
woojiahao <woojiahao1234@gmail.com>
2019-02-25 12:06:43 +0800

    Cleaned up MarkdownConverter to reduce code length - moved the \n{string}\n pattern to a method, style -> documentStyle to prevent overlapping names

--- src/main/kotlin/com/github/woojiahao/MarkdownConverter.kt
+++ src/main/kotlin/com/github/woojiahao/MarkdownConverter.kt
@@ -15,7 +15,7 @@ import com.github.kittinunf.result.Result as KResult
 
 class MarkdownConverter private constructor(
   private val markdownDocument: MarkdownDocument,
-  private val style: Style,
+  private val documentStyle: Style,
   private val targetLocation: File
 ) {
 
@@ -30,9 +30,9 @@ class MarkdownConverter private constructor(
 
   fun convert(): KResult<File, Exception> {
     with(ITextRenderer()) {
-      println(style.getStyles())
+      println(generateHtml())
 
-      setDocumentFromString(generateMarkdownHTML())
+      setDocumentFromString(generateHtml())
       loadFontDirectories()
       layout()
       return try {
@@ -44,19 +44,21 @@ class MarkdownConverter private constructor(
     }
   }
 
-  fun generateMarkdownHTML() =
+  fun generateHtml() =
     StringBuilder()
       .appendHTML()
       .html {
         head {
           style {
-            unsafe { +"\n${this@MarkdownConverter.style.getStyles()}\n" }
+            unsafe { +wrapHtmlContent(documentStyle.getStyles()) }
           }
         }
         body {
-          unsafe { +"\n${htmlRenderer.render(markdownDocument.parsedDocument).trim()}\n" }
+          unsafe { +wrapHtmlContent(htmlRenderer.render(markdownDocument.parsedDocument).trim()) }
         }
       }.toString()
+
+  private fun wrapHtmlContent(content: String) = "\n$content\n"
 
   private fun ITextRenderer.loadFontDirectories() {
     val fontDirectories = getFontDirectories()
