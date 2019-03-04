541d6f5a836fb04ded7eee1007ef231d1480f670
woojiahao <woojiahao1234@gmail.com>
2019-03-04 16:50:50 +0800

    Updated default code style to be more neutral

--- src/main/kotlin/com/github/woojiahao/style/elements/code/Code.kt
+++ src/main/kotlin/com/github/woojiahao/style/elements/code/Code.kt
@@ -10,7 +10,7 @@ open class Code(
   fontSize: Double,
   fontFamily: FontFamily
 ) : Element(elementName, fontSize, fontFamily) {
-  override var textColor = c("FF3D00")
-  override var backgroundColor = c("#F5F5F5")
+  override var textColor = c("616161")
+  override var backgroundColor = c("F5F5F5")
   override var padding: Box<Double>? = Box(10.0)
 }
