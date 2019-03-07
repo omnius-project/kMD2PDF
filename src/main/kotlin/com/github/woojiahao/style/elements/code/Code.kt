3fe5f527b5f487dc97da1b8132706ea75fcfcc97
woojiahao <woojiahao1234@gmail.com>
2019-03-07 12:45:36 +0800

    Fixed inline code block styling

--- src/main/kotlin/com/github/woojiahao/style/elements/code/Code.kt
+++ src/main/kotlin/com/github/woojiahao/style/elements/code/Code.kt
@@ -8,7 +8,6 @@ import com.github.woojiahao.utility.c
 
 open class Code(elementName: String, settings: Settings) : Element(elementName, settings) {
   override var fontFamily = settings.monospaceFont
-  override var textColor = c("616161")
+  override var textColor = c("E65100")
   override var backgroundColor = c("F5F5F5")
-  override var padding: Box<Double>? = Box(10.0)
 }
