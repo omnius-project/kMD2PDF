8c4aa337f470c0e4703ccbc9317fa1931d1f6415
woojiahao <woojiahao1234@gmail.com>
2019-03-07 11:20:34 +0800

    Added theme property to Settings class to toggle the theming system

--- src/main/kotlin/com/github/woojiahao/style/Settings.kt
+++ src/main/kotlin/com/github/woojiahao/style/Settings.kt
@@ -5,9 +5,17 @@ import com.github.woojiahao.style.utility.FontFamily.BaseFontFamily.MONOSPACE
 import com.github.woojiahao.style.utility.FontFamily.BaseFontFamily.SANS_SERIF
 
 class Settings {
+  enum class Theme {
+    DARK, LIGHT
+  }
+
   var fontSize = 16.0
+
+  var theme = Theme.LIGHT
+
   var font = FontFamily(SANS_SERIF)
     get() = field.clone()
+
   var monospaceFont = FontFamily(MONOSPACE)
     get() = field.clone()
 }
