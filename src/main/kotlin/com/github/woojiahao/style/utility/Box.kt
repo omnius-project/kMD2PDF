77d0d1e566ae04e589fa46d053764ce41486d397
woojiahao <woojiahao1234@gmail.com>
2019-03-12 16:46:21 +0800

    Refactored Box
    Improved readability of the modification toCss overload, added a toCss
    overload to print a set of items in a certain order

--- src/main/kotlin/com/github/woojiahao/style/utility/Box.kt
+++ src/main/kotlin/com/github/woojiahao/style/utility/Box.kt
@@ -1,14 +1,20 @@
 package com.github.woojiahao.style.utility
 
-/**
- * Box of measurements for all directions.
- */
 open class Box<T>(val top: T, val right: T, val bottom: T, val left: T) {
+
   constructor(vertical: T, horizontal: T) : this(vertical, horizontal, vertical, horizontal)
   constructor(all: T) : this(all, all, all, all)
 
-  fun toCss() = "$top $right $bottom $left"
+  fun toCss() = toCss(top, right, bottom, left)
 
-  fun <P> toCss(modification: (T) -> P) =
-    "${modification(top)} ${modification(right)} ${modification(bottom)} ${modification(left)}"
+  fun <P> toCss(modification: (T) -> P): String {
+    val modifiedTop = modification(top)
+    val modifiedRight = modification(right)
+    val modifiedBottom = modification(bottom)
+    val modifiedLeft = modification(left)
+
+    return toCss(modifiedTop, modifiedRight, modifiedBottom, modifiedLeft)
+  }
+
+  fun <P> toCss(vararg items: P) = items.joinToString(" ")
 }
