bbfa9dc70f647417742187e61af4515dac7b73a0
woojiahao <woojiahao1234@gmail.com>
2019-03-20 15:19:55 +0800

    Completed c function extension
    c function now accepts 2 or 3 characters, repeating them thrice or twice
    if necessary. Unit tests updated to reflect this change

--- src/main/kotlin/com/github/woojiahao/utility/StyleUtility.kt
+++ src/main/kotlin/com/github/woojiahao/utility/StyleUtility.kt
@@ -4,20 +4,35 @@ import java.awt.Color
 
 /**
  * Returns a matching [Color] based on the given [hexCode].
+ *
+ * CSS color shortcuts are supported as well:
+ *
+ * - c("FA")      => #FAFAFA
+ * - c("FB1")     => #FB1FB1
+ * - c("FAF6B1")  => #FAF6B1
  */
 fun c(hexCode: String): Color? {
   val convertedHexCode = hexCode.replace("#", "").toLowerCase()
+  val convertedHexCodeLength = convertedHexCode.length
 
-  require(convertedHexCode.length == 6) { "Invalid hex code length (${convertedHexCode.length})" }
+  val allowedLength = listOf(2, 3, 6)
 
-  convertedHexCode.forEach {
+  require(convertedHexCodeLength in allowedLength) {
+    "Invalid hex code length (${convertedHexCode.length}), accepted length include 2, 3 and 6"
+  }
+
+  val repetitions = 6 / convertedHexCodeLength
+  val finalHexCode = convertedHexCode.repeat(repetitions)
+
+  finalHexCode.forEach {
     require(it.isLetterOrDigit()) { "Invalid character ($it) in hex code" }
+
     if (it.isLetter()) {
       require(it in 'a'..'f') { "Invalid character ($it)" }
     }
   }
 
-  return Color.decode("#$convertedHexCode")
+  return Color.decode("#$finalHexCode")
 }
 
 /**
