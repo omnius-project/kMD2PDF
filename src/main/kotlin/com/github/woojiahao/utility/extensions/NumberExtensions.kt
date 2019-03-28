5a587ca76a16f1a35d834f91a0b686216bfcdd38
woojiahao <woojiahao1234@gmail.com>
2019-03-28 14:01:14 +0800

    Removed NumberExtensions since not used

--- src/main/kotlin/com/github/woojiahao/utility/extensions/NumberExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.github.woojiahao.utility.extensions
-
-import com.github.woojiahao.style.utility.`in`
-
-fun Double.toInches() = (this / 25.4).`in`
-
-fun Int.toInches() = (this / 25.4).`in`
