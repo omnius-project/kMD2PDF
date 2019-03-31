db0866f86b53c36adb64d0fe183e4a8a6a86af01
woojiahao <woojiahao1234@gmail.com>
2019-03-31 11:54:51 +0800

    Added +/- unit tests
    Added the unit tests for the overridden binary plus and minus operators
    for the Measurement class

--- src/test/kotlin/com/github/woojiahao/style/utility/MeasurementTest.kt
+++ src/test/kotlin/com/github/woojiahao/style/utility/MeasurementTest.kt
@@ -95,9 +95,47 @@ class MeasurementTest {
 
   @Test
   fun `toString returns Measurement in CSS format`() {
+    testAllMeasurements { type, num, measurement ->
+      assertEquals("$num${type.measurement}", measurement.toString())
+    }
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun `+ throws IllegalArgumentException if type of Measurements do not match`() {
+    val first = 10.px
+    val second = 10.`in`
+    first + second
+  }
+
+  @Test
+  fun `+ adds the values of both Measurements, returning a new Measurement`() {
+    val first = 25.px
+    val second = 39.px
+    val final = first + second
+    val expectedMeasurement = (25 + 39).px
+    assertEquals(expectedMeasurement, final)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun `- throws IllegalArgumentException if type of Measurements do not match`() {
+    val first = 10.px
+    val second = 10.cm
+    first - second
+  }
+
+  @Test
+  fun `- subtracts the values of both Measurements, returning a new Measurement`() {
+    val first = 57.px
+    val second = 12.px
+    val final = first - second
+    val expectedMeasurement = (57 - 12).px
+    assertEquals(expectedMeasurement, final)
+  }
+
+  private fun testAllMeasurements(assert: (Measurement.Type, Number, Measurement<*>?) -> Unit) {
     Measurement.Type.values().forEach {
       testPossibleInputs(it) { num, measurement ->
-        assertEquals("$num${it.measurement}", measurement.toString())
+        assert(it, num, measurement)
       }
     }
   }
