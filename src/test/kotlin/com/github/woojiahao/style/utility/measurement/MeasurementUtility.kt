e5c8a4975be3076d909e8784fc5db4d623648249
woojiahao <woojiahao1234@gmail.com>
2019-04-02 09:57:29 +0800

    Completed some Measurement#convert tests

--- src/test/kotlin/com/github/woojiahao/style/utility/measurement/MeasurementUtility.kt
+++ src/test/kotlin/com/github/woojiahao/style/utility/measurement/MeasurementUtility.kt
@@ -2,6 +2,7 @@ package com.github.woojiahao.style.utility.measurement
 
 import com.github.woojiahao.style.utility.Measurement
 import com.github.woojiahao.style.utility.match
+import kotlin.test.assert
 
 private val potentialIntInputs = listOf(Int.MIN_VALUE, Int.MAX_VALUE, 0, 123456, -123456)
 private val potentialDoubleInputs = listOf(Double.MIN_VALUE, Double.MAX_VALUE, 0.0, 1234.56, -1234.56)
@@ -9,7 +10,7 @@ private val potentialFloatInputs = listOf(Float.MIN_VALUE, Float.MAX_VALUE, 0.0f
 private val potentialShortInputs = listOf(Short.MIN_VALUE, Short.MAX_VALUE, 0, 1234, -1234)
 private val potentialByteInputs = listOf(Byte.MIN_VALUE, Byte.MAX_VALUE, 0, 12, -12)
 private val potentialLongInputs = listOf(Long.MIN_VALUE, Long.MAX_VALUE, 0, 12345678, -12345678)
-private val allInputs: List<List<Number>> = listOf(
+val allInputs: List<List<Number>> = listOf(
   potentialByteInputs,
   potentialShortInputs,
   potentialIntInputs,
@@ -39,6 +40,23 @@ fun assertMeasurement(type: Measurement.Type, assert: (Number, Measurement<*>?) 
         else -> null
       }
       assert(num, measurement)
+    }
+  }
+}
+
+fun assertMeasurementIndexed(type: Measurement.Type, assert: (Pair<Int, Int>, Number, Measurement<*>?) -> Unit) {
+  allInputs.forEachIndexed { outerIndex, list ->
+    list.forEachIndexed { innerIndex, num ->
+      val measurement = when (num) {
+        is Int -> num match type
+        is Long -> num match type
+        is Double -> num match type
+        is Float -> num match type
+        is Short -> num match type
+        is Byte -> num match type
+        else -> null
+      }
+      assert(outerIndex to innerIndex, num, measurement)
     }
   }
 }
