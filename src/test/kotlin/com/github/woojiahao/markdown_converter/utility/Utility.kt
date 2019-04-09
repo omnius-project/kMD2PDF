c8b588c20e92e7b30b461ff2cd679a685b789146
woojiahao <woojiahao1234@gmail.com>
2019-04-09 16:38:46 +0800

    Created error message for child count fail
    Created error message function for generating child count assertion
    fails so that the messages are more descriptive

--- src/test/kotlin/com/github/woojiahao/markdown_converter/utility/Utility.kt
+++ src/test/kotlin/com/github/woojiahao/markdown_converter/utility/Utility.kt
@@ -2,17 +2,12 @@ package com.github.woojiahao.markdown_converter.utility
 
 import com.github.woojiahao.MarkdownDocument
 import com.github.woojiahao.markdownConverter
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.io.File
 import kotlin.test.assertEquals
 
 private val resourcesDirectory
   get() = File("src/test/resources/markdown-converter")
-
-val Element.childCount
-  get() = children().size
 
 fun getResource(folder: String, file: String, extension: String? = null): File {
   val fileName = extension?.let { "$file.$it" } ?: file
@@ -37,13 +32,19 @@ fun assertMarkdown(folder: String, file: String) {
   val actualDocument = parseDocument(converter.generateBody()).getElementsByClass("content").first()
 
   // Ensure that they both have the same number of children
-  assertEquals(expectedDocument.childCount, actualDocument.childCount, "Document child count don't match")
+  assertEquals(
+    expectedDocument.childCount,
+    actualDocument.childCount,
+    generateChildCountAssertFalseMessage(expectedDocument, actualDocument, "Document child count don't match")
+  )
 
   // Ensure that they both have the same set of elements
   val expectedDocumentBody = expectedDocument.children()
   val actualDocumentBody = actualDocument.children()
 
-  expectedDocumentBody.zip(actualDocumentBody).forEach { compare(it.first, it.second) }
+  expectedDocumentBody.zip(actualDocumentBody).forEach {
+    compare(it.first, it.second)
+  }
 }
 
 private fun compare(ex: Element, ac: Element) {
@@ -57,7 +58,11 @@ private fun compare(ex: Element, ac: Element) {
 }
 
 private fun assertElementEquals(ex: Element, ac: Element) {
-  assertEquals(ex.childCount, ac.childCount, "Element child count don't match")
+  assertEquals(
+    ex.childCount,
+    ac.childCount,
+    generateChildCountAssertFalseMessage(ex, ac, "Element child count don't match")
+  )
   assertEquals(ex.tagName(), ac.tagName(), "Element tags don't match")
   assertEquals(ex.ownText(), ac.ownText(), "Element text don't match")
   assertEquals(ex.attributes().size(), ac.attributes().size(), "Element attribute size don't match")
@@ -72,8 +77,9 @@ private fun assertElementEquals(ex: Element, ac: Element) {
   }
 }
 
-private fun parseDocument(html: String): Document =
-  Jsoup.parse(html).apply {
-    outputSettings().syntax(Document.OutputSettings.Syntax.xml)
-    html().replace("&nbsp;", " ")
-  }
+private fun generateChildCountAssertFalseMessage(ex: Element, ac: Element, prepend: String = "") =
+  mutableListOf<String>().apply {
+    if (prepend.isNotEmpty()) this += prepend
+    this += "Expected: ${ex.childrenNames.joinToString(", ")}"
+    this += "Actual: ${ac.childrenNames.joinToString(", ")}"
+  }.joinToString("\n")
