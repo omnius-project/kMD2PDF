6e0ce6ad5bc61e99ced5e177ed3c570679dc6be5
woojiahao <woojiahao1234@gmail.com>
2019-04-09 08:59:57 +0800

    Added unit test for absolute local file paths
    Added @Ignore annotation to unit test since it's not able to be tested
    with Travis, but the test passes on my local machine

--- src/test/kotlin/com/github/woojiahao/markdown_converter/FigureElementTest.kt
+++ src/test/kotlin/com/github/woojiahao/markdown_converter/FigureElementTest.kt
@@ -1,5 +1,6 @@
 package com.github.woojiahao.markdown_converter
 
+import org.junit.Ignore
 import org.junit.Test
 
 class FigureElementTest : ElementTest("figure") {
@@ -10,6 +11,16 @@ class FigureElementTest : ElementTest("figure") {
   fun `Alt text renders to figure`() = assert("AltText")
 
   @Test
-  fun `Absolute web paths will generate a figure with src set to web path`() = assert("AbsoluteWebPath")
+  fun `Absolute web paths is used as src`() = assert("AbsoluteWebPath")
 
+  /**
+   * Ignoring this test until absolute file path can be handled via Travis and in the markdown.
+   *
+   * Running it locally on my Windows machine works.
+   *
+   * TODO: Figure out how to create absolute file paths in the markdown document
+   */
+  @Test
+  @Ignore
+  fun `Local absolute path is used as src`() = assert("AbsoluteLocalPath")
 }
