ff757a693e932e5ae3e71dbdc7bc6d520f35333f
woojiahao <woojiahao1234@gmail.com>
2019-04-13 16:09:25 +0800

    Added simple unit test for MarkdownConverter

--- src/test/kotlin/com/github/woojiahao/markdown_converter/MarkdownConverterTest.kt
+++ src/test/kotlin/com/github/woojiahao/markdown_converter/MarkdownConverterTest.kt
@@ -1,5 +1,29 @@
 package com.github.woojiahao.markdown_converter
 
+import com.github.woojiahao.MarkdownDocument
+import com.github.woojiahao.markdownConverter
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
 class MarkdownConverterTest {
 
+  @get:Rule
+  val folder = TemporaryFolder()
+
+  @Test(expected = IllegalStateException::class)
+  fun `IllegalStateException thrown if MarkdownDocument is not supplied`() {
+    markdownConverter {}
+  }
+
+  @Test
+  fun `MarkdownDocument must be supplied`() {
+    val markdownFile = folder.newFile("README.md").also {
+      it.writeText("# Hello")
+    }
+    val markdownDocument = MarkdownDocument(markdownFile)
+    markdownConverter {
+      document(markdownDocument)
+    }
+  }
 }
